a862c75749bef607abce928d68b0ca8e53c705bc
FUJI0130-TIA <shinya_fujita@tiasahi.com>
2023-04-20 14:32:29 +0900

    一旦STEP５を終了とする

--- MAUI3rdTest/Views/SamplePageView.xaml.cs
+++ MAUI3rdTest/Views/SamplePageView.xaml.cs
@@ -41,42 +41,13 @@ public partial class SamplePageView : ContentPage
 
     private async  void Test_Click_CheckDBdata(object sender, EventArgs args)
     {
-        //List<PhotoTables> result = _viewModel._Test_tairaba.GetDBdatas();
-        //coll.ItemsSource = result;//ここでwatchしたら、中身入ってたけど、画面には何も表示されなかった
-
         _viewModel._photoDbContext = new PhotoDbContext();
 
         var items = await _viewModel._photoDbContext.Photos.ToListAsync();//この中にデータ入ってるのは確認できた
-        //var items = await _viewModel._photoDbContext.PhotosData.ToListAsync();//この中にデータ入ってるのは確認できた
 
         ObservableCollection<Photos> items_obsv = new ObservableCollection<Photos>(items);
 
-        //一回viewModelの中にべた書きでプロパティに値設定してみる
-
-        //確認はできたけど意味ない。
-        //foreach(Photos photos in items)
-        //{
-        //    _viewModel.写真ID = photos.写真ID;
-        //    //_viewModel.PhotoID = photos.PhotoID;//test //あかんかった
-        //    _viewModel.FishID_Text = photos.釣れた魚ID;
-        //    _viewModel.TypeFishID_Text = photos.釣り物ID;
-        //    _viewModel.TairabaDataID_Text = photos.タイラバデータID;
-        //    _viewModel.Weather_Text = photos.天気;
-        //    _viewModel.DataDate_Text = photos.日付;
-        //    _viewModel.Temperture_Text = photos.水温.ToString();
-        //    _viewModel.UserID_Text = photos.ユーザーID.ToString();
-        //}
-
-
-        //_viewModel._items = items;//ここまでは中身入ってるのが確認できてる　この後のバインディングがうまくいかない
-        _viewModel._items = items_obsv;//ここまでは中身入ってるのが確認できてる　この後のバインディングがうまくいかない
-
-        coll.ItemsSource = _viewModel._items;//これ書かないと画面にListの表示されないっぽい
-        //coll.ItemsSource = items;//
-
-        //どっちでやってもバインドエラーの内容同じ
-
-
+        coll.ItemsSource = items_obsv;
 
     }
 }
